20ae5220fa2b00f87c754af3ebd85779e043974a
arps18 <arpanp99@gmail.com>
2020-09-10 20:16:58 +0530

    Navbar Done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    if (scroll < 300) {
+    if (scroll < 600) {
         $('.fixed-top').css('background', 'transparent');
     } else {
         $('.fixed-top').css('background', 'rgba(23, 162, 184, 0.9)');
